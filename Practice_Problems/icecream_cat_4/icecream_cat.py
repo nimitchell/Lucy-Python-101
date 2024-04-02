a8d4453748be18d6fec6e6e8353eeb0397b72e19
Nicholas <nmitchell93099@gmail.com>
2024-04-02 01:28:14 -0400

    added icecream cat problem

---
+++ Practice_Problems/icecream_cat_4/icecream_cat.py
@@ -0,0 +1,9 @@
+"""
+######################################################################################################################################
+Use pandas to make a dataframe of the cat icecream csv, https://figshare.com/articles/dataset/cat_icecream_dataset/2059248
+
+Then group the cats by color, and print out the favorite icecream flavor of each color of cat
+
+Finaly use matplotlib.pyplot to graph popularity of different ice cream flavors for orange cats
+######################################################################################################################################
+"""
